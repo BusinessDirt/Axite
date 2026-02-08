ea8f7641ba8b89d5947596506d0f3aed8ef2b6df
Maximilian Bollschweiler <maxi.bollschweiler@gmail.com>
2026-02-08 18:41:09 +0100

    refactor(commands): simplify ArgumentBuilderTest

--- commands/src/test/kotlin/github/businessdirt/axite/commands/builder/ArgumentBuilderTest.kt
+++ commands/src/test/kotlin/github/businessdirt/axite/commands/builder/ArgumentBuilderTest.kt
@@ -4,7 +4,6 @@ import github.businessdirt.axite.commands.Command
 import github.businessdirt.axite.commands.arguments.IntegerArgumentType
 import github.businessdirt.axite.commands.nodes.CommandNode
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.mockito.Mockito.mock
 import java.util.function.Predicate
@@ -12,19 +11,13 @@ import kotlin.test.Test
 
 @DisplayName("ArgumentBuilder logic tests")
 class ArgumentBuilderTest {
-    private lateinit var builder: TestableArgumentBuilder<Any>
-
-    @BeforeEach
-    fun setUp() {
-        builder = TestableArgumentBuilder()
-    }
 
     @Test
     @DisplayName("apply { argument } should successfully add a child argument")
     fun testArguments() {
-        builder.apply { argument("bar", IntegerArgumentType()) }
-
+        val builder = testBuilder { argument("bar", IntegerArgumentType()) }
         val builtArgument = argument<Any, Int>("bar", IntegerArgumentType())
+
         assertEquals(1, builder.allArguments.size)
         assertTrue(builder.allArguments.contains(builtArgument), "Builder should contain the added argument")
     }
@@ -33,18 +26,17 @@ class ArgumentBuilderTest {
     @DisplayName("Should throw if a redirect is already set")
     fun testThen_withRedirect() {
         val target: CommandNode<Any> = mock()
-        builder.redirect(target)
+        val builder = testBuilder { redirect(target) }
 
-        assertThrows(IllegalStateException::class.java) {
-            builder.literal("foo")
-        }
+        assertThrows(IllegalStateException::class.java) { builder.literal("foo") }
     }
 
     @Test
     @DisplayName("redirect() should correctly set the target node")
     fun testRedirect() {
         val target: CommandNode<Any> = mock()
-        builder.redirect(target)
+        val builder = testBuilder { redirect(target) }
+
         assertEquals(target, builder.testRedirect)
     }
 
@@ -52,23 +44,23 @@ class ArgumentBuilderTest {
     @DisplayName("redirect() should throw if children already exist")
     fun testRedirect_withChild() {
         val target: CommandNode<Any> = mock()
-        builder.apply {
-            literal("foo")
-        }
+        val builder = testBuilder { literal("foo") }
 
-        assertThrows(IllegalStateException::class.java) {
-            builder.redirect(target)
-        }
+        assertThrows(IllegalStateException::class.java) { builder.redirect(target) }
+    }
+
+    private fun testBuilder(block: TestableArgumentBuilder.() -> Unit): TestableArgumentBuilder {
+        return TestableArgumentBuilder().apply(block)
     }
 
     /**
      * Test implementation of the abstract ArgumentBuilder.
      */
-    private class TestableArgumentBuilder<S> : ArgumentBuilder<S, TestableArgumentBuilder<S>>() {
-        override val self: TestableArgumentBuilder<S>
+    private class TestableArgumentBuilder : ArgumentBuilder<Any, TestableArgumentBuilder>() {
+        override val self: TestableArgumentBuilder
             get() = this
 
-        override fun build(): CommandNode<S> = mock()
+        override fun build(): CommandNode<Any> = mock()
     }
 }
 
