417b75f9513f25c9e39eb9f67cfb94f2a23f661a
Maximilian Bollschweiler <maxi.bollschweiler@gmail.com>
2026-06-03 10:38:05 +0200

    feat(vanadium): add a PostProcessingRenderer

---
+++ vanadium/src/main/kotlin/github/businessdirt/axite/vanadium/renderer/PostProcessingRenderer.kt
@@ -0,0 +1,129 @@
+package github.businessdirt.axite.vanadium.renderer
+
+import github.businessdirt.axite.vanadium.Vanadium
+import github.businessdirt.axite.vanadium.assets.types.Shader
+import github.businessdirt.axite.vanadium.core.profiling.Profiler
+import github.businessdirt.axite.vanadium.core.utils.memoryStack
+import github.businessdirt.axite.vanadium.vulkan.Context
+import github.businessdirt.axite.vanadium.vulkan.commands.CommandBuffer
+import github.businessdirt.axite.vanadium.vulkan.commands.bindDescriptorSets
+import github.businessdirt.axite.vanadium.vulkan.commands.draw
+import github.businessdirt.axite.vanadium.vulkan.descriptors.DescriptorPool
+import github.businessdirt.axite.vanadium.vulkan.descriptors.DescriptorSet
+import github.businessdirt.axite.vanadium.vulkan.pipeline.GraphicsPipeline
+import github.businessdirt.axite.vanadium.vulkan.resources.Attachment
+import github.businessdirt.axite.vanadium.vulkan.resources.Buffer
+import github.businessdirt.axite.vanadium.vulkan.resources.Sampler
+import org.lwjgl.system.MemoryUtil
+import org.lwjgl.vulkan.VK13.*
+
+class PostProcessingRenderer(val context: Context) {
+
+    companion object {
+        const val VERTEX_SHADER_PATH = "src/main/resources/shaders/post.vert.glsl"
+        const val FRAGMENT_SHADER_PATH = "src/main/resources/shaders/post.frag.glsl"
+    }
+
+    private var graphicsPipeline: GraphicsPipeline? = null
+    private var descriptorPool: DescriptorPool? = null
+    private var imageSet: DescriptorSet? = null
+    private var screenSizeSet: DescriptorSet? = null
+    private var sampler: Sampler? = null
+    private var screenSizeBuffer: Buffer? = null
+
+    var effectType: Int = 0
+        set(value) {
+            if (field != value) {
+                field = value
+                recreatePipeline()
+            }
+        }
+
+    var useFxaa: Boolean = false
+        set(value) {
+            if (field != value) {
+                field = value
+                recreatePipeline()
+            }
+        }
+
+    suspend fun initialize() = Profiler.profile("PostProcessingRenderer Initialization") {
+        val vertexShader = Vanadium.assets.load<Shader>(VERTEX_SHADER_PATH)
+        val fragmentShader = Vanadium.assets.load<Shader>(FRAGMENT_SHADER_PATH)
+
+        sampler = Sampler(context.device.handle) {
+            magFilter = VK_FILTER_LINEAR
+            minFilter = VK_FILTER_LINEAR
+            addressModeU = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE
+            addressModeV = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE
+        }
+
+        recreatePipeline(vertexShader, fragmentShader)
+
+        descriptorPool = DescriptorPool(
+            context.device.handle, 2, listOf(
+                DescriptorPool.PoolSize(VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1),
+                DescriptorPool.PoolSize(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1)
+            )
+        )
+
+        screenSizeBuffer = Buffer(
+            context.device.handle,
+            context.physicalDevice,
+            8, // vec2 (float, float)
+            VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+            VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT or VK_MEMORY_PROPERTY_HOST_COHERENT_BIT
+        )
+
+        graphicsPipeline?.layout?.descriptorSetLayouts?.let { layouts ->
+            imageSet = DescriptorSet(context.device.handle, descriptorPool!!, layouts[0])
+            screenSizeSet = DescriptorSet(context.device.handle, descriptorPool!!, layouts[1])
+            screenSizeSet?.updateBuffer(0, screenSizeBuffer!!.handle, 8)
+        }
+    }
+
+    private fun recreatePipeline(
+        vertexShader: Shader? = graphicsPipeline?.configuration?.vertexShader,
+        fragmentShader: Shader? = graphicsPipeline?.configuration?.fragmentShader
+    ) {
+        val oldPipeline = graphicsPipeline
+        graphicsPipeline = GraphicsPipeline {
+            this.vertexShader(vertexShader!!)
+            this.fragmentShader(fragmentShader!!)
+            specializationConstant("EFFECT_TYPE", effectType)
+            specializationConstant("USE_FXAA", if (useFxaa) 1 else 0)
+        }
+        oldPipeline?.close()
+    }
+
+    fun render(commandBuffer: CommandBuffer, input: Attachment) {
+        val pipeline = graphicsPipeline ?: return
+        val imgSet = imageSet ?: return
+        val sizeSet = screenSizeSet ?: return
+
+        // Update screen size
+        val map = screenSizeBuffer!!.map()
+        val buffer = MemoryUtil.memByteBuffer(map, 8)
+        buffer.putFloat(input.width.toFloat())
+        buffer.putFloat(input.height.toFloat())
+        screenSizeBuffer!!.unmap()
+
+        imgSet.updateImage(0, input.imageView.handle, sampler!!.handle)
+
+        pipeline.bind(commandBuffer)
+        commandBuffer.bindDescriptorSets(pipeline.layout.handle, longArrayOf(imgSet.handle, sizeSet.handle))
+        commandBuffer.draw(3)
+    }
+
+    fun shutdown() {
+        imageSet?.close()
+        screenSizeSet?.close()
+        descriptorPool?.close()
+        sampler?.close()
+        screenSizeBuffer?.close()
+        graphicsPipeline?.close()
+
+        Vanadium.assets.unload(VERTEX_SHADER_PATH)
+        Vanadium.assets.unload(FRAGMENT_SHADER_PATH)
+    }
+}
